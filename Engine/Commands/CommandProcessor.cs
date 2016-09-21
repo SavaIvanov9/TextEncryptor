fc05173792b548fd417a0bd3adee50a8257eecc3
SavaIvanov9 <sava.ivanov.9@gmail.com>
2016-09-21 05:02:10 +0300

    Basic structure for commands added

--- Engine/Commands/CommandProcessor.cs
+++ Engine/Commands/CommandProcessor.cs
@@ -1,12 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextEncryptor.Engine.Commands
 {
-    class CommandProcessor
+    public class CommandProcessor
     {
+        private static CommandProcessor _instance;
+
+        public static CommandProcessor Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CommandProcessor();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void ProcessCommand(string command, string[] commandParams)
+        {
+            switch (command)
+            {
+                case Constants.Help:
+                    this.DisplayCommands();
+                    break;
+                case Constants.EncryptString:
+                    this.EncryptString();
+                    break;
+                case Constants.DecryptStryng:
+                    this.DecryptString();
+                    break;
+                case Constants.EncryptTxtFile:
+                    this.EncryptTxtFile();
+                    break;
+                case Constants.DecryptTxtFile:
+                    this.DecryptTxtFile();
+                    break;
+                case Constants.ClearConsole:
+                    this.ClearConsole();
+                    break;
+                default:
+                    Console.WriteLine(@"Unknown command. Type ""help"" for a list of commands.");
+                    break;
+            }
+        }
+
+        private void ClearConsole()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DecryptTxtFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EncryptTxtFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DecryptString()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EncryptString()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DisplayCommands()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Engine/Commands/Constants.cs
+++ Engine/Commands/Constants.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextEncryptor.Engine.Commands
+﻿namespace TextEncryptor.Engine.Commands
 {
-    class Constants
+    internal class Constants
     {
+        public static string[] ValidChoises = {
+            "help",
+            "encrypt string",
+            "decrypt string",
+            "encrypt txt file",
+            "decrypt txt file",
+            "clear"};
+
+        public const string Help = "help";
+        public const string EncryptString = "encrypt string";
+        public const string DecryptStryng = "decrypt string";
+        public const string EncryptTxtFile = "encrypt txt file";
+        public const string DecryptTxtFile = "decrypt txt file";
+        public const string ClearConsole = "clear";
     }
 }
+
